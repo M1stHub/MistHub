fe17693ce20a90bc15f8147a2e5ecfdf4e18e674
Aentix <56798290+xAentix@users.noreply.github.com>
2023-11-17 03:19:54 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,9 @@
-
+if game.PlaceId == 10595058975 then
+    if getgenv().Test == true then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xAentix/MistHub/main/ArcaneLineageTest.lua", true))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xAentix/MistHub/main/ArcaneLineage.lua", true))()
+    end
+else
+    game.Players.LocalPlayer:Kick("Game not supported")
+end
